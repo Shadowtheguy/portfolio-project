92cc206334afb5cb18911556f0542eb8fa960443
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-10-02 21:14:54 -0500

    Personalized the website

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,30 @@ function App() {
           <>
           <ProjectCard children={
             <>
-            <ProjectImage />
-            <ProjectTitle />
-            <ProjectDescription />
+            <ProjectImage imagesrc={"src/assets/madLibs.jpg"} imagename={"Mad Libs"}/>
+            <ProjectTitle title={"Mad Libs Site"}/>
+            <ProjectDescription description={"Website made to show off my knowledge of objects and server creation."}/>
+            </>
+          } />
+          <ProjectCard children={
+            <>
+            <ProjectImage imagesrc={"src/assets/dndSite.png"} imagename={"Dnd Dice"}/>
+            <ProjectTitle title={"Dnd Character Helper"}/>
+            <ProjectDescription description={"A capstone project to show my knowledge of both JavaScript and HTML"}/>
+            </>
+          } />
+          <ProjectCard children={
+            <>
+            <ProjectImage imagesrc={"src/assets/triviaSite.jpg"} imagename={"Trivia"}/>
+            <ProjectTitle title={"Trivia Bot"}/>
+            <ProjectDescription description={"A project to show my ability to incorporate a chatbot"}/>
+            </>
+          } />
+          <ProjectCard children={
+            <>
+            <ProjectImage imagesrc={"src/assets/waterSite.jpg"} imagename={"Glass of Water"} />
+            <ProjectTitle title={"Water Conservation"} />
+            <ProjectDescription description={"A project to show basic knowledge of javascript and html elements, one of my first."} />
             </>
           } />
           </>
