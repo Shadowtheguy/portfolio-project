699f6a6edfcf037f39c49cab64ff4c28ecf6e487
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-10-02 20:20:21 -0500

    Created the Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return (
+    <>
+        <h1>MYLOGO.COM</h1>
+        <hr />
+
+        <h2>PORTFOLIO</h2>
+        <p>Resize the browser window to see the responsive effect.</p>
+    </>
+    );
+};
